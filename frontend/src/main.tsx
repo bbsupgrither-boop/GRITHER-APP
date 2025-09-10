cd3cfd63c736e1c879d7705c7d934c1ce286c61c
bbsupgrither-boop <bbsupgrither-boop@users.noreply.github.com>
2025-09-10 21:35:16 +0300

    олностью пересоздан фронтенд с нуля - простая сборка работает

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,22 +1,9 @@
 ﻿import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-
-// ункция для скрытия прелоадера
-const hideLoader = () => {
-  const loader = document.getElementById('app-loader');
-  if (loader) {
-    loader.style.opacity = '0';
-    loader.style.transition = 'opacity 0.5s ease';
-    setTimeout(() => loader.remove(), 500);
-  }
-};
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-);
-
-// Скрываем прелоадер после рендера
-setTimeout(hideLoader, 500);
+)
